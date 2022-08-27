e3fa9f92eaa0b9225a611332b8b3d7583726513b
Ahmed Esmat <ahmedesmat01@yahoo.co.uk>
2022-08-27 16:38:23 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" /> <br/>
+          <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
